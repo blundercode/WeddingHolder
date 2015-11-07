b4de85b824abf421ebb42cf54076ffd2d30d0ed1
blundercode <erik.mcland@gmail.com>
2015-11-07 16:37:01 -0700

    fixed bugs

--- js/main.js
+++ js/main.js
@@ -40,35 +40,35 @@
 			
 		/* Gallery
 		================================================== */
-		new Photostack( document.getElementById( 'photostack' ), {
-			callback : function( item ) {
-				//console.log(item)
-			}
-		} );	
+		// new Photostack( document.getElementById( 'photostack' ), {
+		// 	callback : function( item ) {
+		// 		//console.log(item)
+		// 	}
+		// } );	
 			
 			/* Gallery popup
 		=================================================== */	
-		$('.photostack').magnificPopup({
-			delegate: 'a',
-			type: 'image',
-			tLoading: 'Loading image #%curr%...',
-			mainClass: 'mfp-img-mobile',
-			gallery: {
-				enabled: true,
-				navigateByImgClick: true,
-				preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-			},
-			image: {
-				tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-				titleSrc: function(item) {
-					return item.el.attr('title');
-				}
-			},
-			/* zoom: {
-				enabled: true,
-				duration: 300 // don't foget to change the duration also in CSS
-			} */
-		});
+		// $('.photostack').magnificPopup({
+		// 	delegate: 'a',
+		// 	type: 'image',
+		// 	tLoading: 'Loading image #%curr%...',
+		// 	mainClass: 'mfp-img-mobile',
+		// 	gallery: {
+		// 		enabled: true,
+		// 		navigateByImgClick: true,
+		// 		preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		// 	},
+		// 	image: {
+		// 		tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+		// 		titleSrc: function(item) {
+		// 			return item.el.attr('title');
+		// 		}
+		// 	},
+		// 	/* zoom: {
+		// 		enabled: true,
+		// 		duration: 300 // don't foget to change the duration also in CSS
+		// 	} */
+		// });
 		
 		//Home Background slider
 		jQuery.supersized({	
